704e939fcf1292d9735fd089c4b8329a281f0e6d
renzo01 <renzogomez577@gmail.com>
2019-11-18 12:18:14 -0500

    Agregacion del controlador

--- CONSULTA/IService1.cs
+++ CONSULTA/IService1.cs
@@ -10,7 +10,7 @@ namespace CONSULTA
 {
     // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IService1" en el código y en el archivo de configuración a la vez.
     [ServiceContract]
-    public interface IService1
+    public interface Consulta
     {
         [OperationContract]
         List<Usuario> ListarUsuarios();
